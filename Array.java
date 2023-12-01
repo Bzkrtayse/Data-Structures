ee6556d0c28b7e3202c4b4783872e6134a951cf1
Ayşe Bozkurt <105844384+Bzkrtayse@users.noreply.github.com>
2023-12-01 17:09:20 +0300

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,66 @@
+
+public class Array {
+
+	private int[] items;
+	private int count;
+
+	public Array(int length) {
+		items = new int[length];
+
+	}
+
+	public void insert(int item) {
+
+		if (items.length == count) {
+
+			int[] newItems = new int[count * 2];
+
+			for (int i = 0; i < count; i++)
+
+				newItems[i] = items[i];
+
+			items = newItems;
+		}
+		items[count++] = item;
+
+	}
+
+	public void delete(int index) {
+		if (index < 0 || index >= count) {
+			throw new IllegalArgumentException();
+		}
+		for (int i = index; i < count; i++) {
+			items[i] = items[i + 1];
+		}
+		count--;
+	}
+
+	public int indexOf(int item) {
+		for (int i = 0; i < count; i++) {
+			if (items[i] == item)
+				return i;
+		}
+		return -1;
+	}
+
+	public void print() {
+		for (int i = 0; i < count; i++) {
+			System.out.println(items[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+		Array numbers = new Array(3);
+
+		numbers.insert(3);
+		numbers.insert(8);
+		numbers.insert(9);
+		numbers.insert(2);
+		numbers.delete(0);
+
+		System.out.println("index of 2: " + numbers.indexOf(2));
+		System.out.println("index of 3: " + numbers.indexOf(3));
+
+		numbers.print();
+	}
+}
